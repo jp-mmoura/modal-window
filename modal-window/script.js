4d43e0c6a1e1bab694e6ef7839e2f6de94371f46
João Pedro <133026499+jp-mmoura@users.noreply.github.com>
2024-07-23 15:22:40 -0300

    Add files via upload

--- modal-window/script.js
+++ modal-window/script.js
@@ -3,9 +3,31 @@
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
-const btnOpenModal = document.querySelector('.show-modal');
+const btnOpenModal = document.querySelectorAll('.show-modal');
 
 
-for (i = 0; i < btnOpenModal.length; i++){
-    console.log(btnOpenModal(i).textContent);
+const openModal = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+for (const element of btnOpenModal){
+    console.log(element.addEventListener('click', openModal));
 };
+
+
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal); 
+
+document.addEventListener('keydown', function(e){
+    if(e.key == 'Escape'){
+        if(!modal.classList.contains('hidden')){
+            closeModal();
+        }
+    };
+});
